fecae3d471cc17ba7fb5b488b3f23152343a83aa
Chu-4hun <Tapok134224@mail.ru>
2023-10-20 12:08:52 +0300

    update: updated submodules and build configs

--- restart.sh
+++ restart.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-echo -e "\033[1;32m \033[41m Updating... \033[0m"
+echo -e "\033[1;32m Updating...\033[1;32m"
 git fetch --all
-git reset --hard origin/master
+git reset --hard origin/main
 git pull
 
 echo -e "\033[1;32m \033[41m Granting rights to bsh scripts... \033[0m"
 # git submodule update --remote --merge
-git submodule update --init --recursive
+git submodule update --recursive
 chmod +x restart.sh
 
 echo -e "\033[1;32m \033[41m Launching a project... \033[0m"
